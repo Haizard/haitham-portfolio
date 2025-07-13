ec2c0ab114b5912309a67d1d4785bedc311bb849
haitham misape <haithammisape@gmail.com>
2025-07-13 09:38:52 +0000

    haitham
    error: cuold not update wishlist: not authenicated

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { getSession } from '@/lib/session';
 
+export const dynamic = 'force-dynamic'; // Prevent caching of this route
+
 export async function GET(request: NextRequest) {
   const session = await getSession();
   
